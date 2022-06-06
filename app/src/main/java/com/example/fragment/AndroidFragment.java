3ba34dc25ab658a65efad12e229912ede0cd0cbf
phi <nguyentruongphi15032003@gmail.com>
2022-06-06 11:34:44 +0700

    random color

--- app/src/main/java/com/example/fragment/AndroidFragment.java
+++ app/src/main/java/com/example/fragment/AndroidFragment.java
@@ -1,5 +1,6 @@
 package com.example.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
 
 
 public class AndroidFragment extends Fragment {
 
+    RelativeLayout relativeLayout;
+    View view;
+    Random random;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_android, container, false);
+        view = inflater.inflate(R.layout.fragment_android, container, false);
+        relativeLayout = view.findViewById(R.id.relative_android);
+        random = new Random();
+        relativeLayout.setBackgroundColor(Color.rgb(random.nextInt() + 1,random.nextInt() + 1,random.nextInt() + 1));
+        return view;
     }
+
 }
